313644287bf06e33cd748c817a19cedcdd8396ea
Aditya J Parida <adityaparida605@gmail.com>
2026-01-15 20:50:49 +0530

    Character Hashing

---
+++ Char_Hash.py
@@ -0,0 +1,11 @@
+s="azyxyyzaaaa"
+q=["d", "a", "y", "x"]
+hash_list=[0] * 26
+for ch in s:
+    ascii_val=ord(ch)
+    index=ascii_val-97
+    hash_list[index]+=1
+for ch in q:
+    ascii_val=ord(ch)
+    index=ascii_val-97
+    print(f"{ch} occurs {hash_list[index]} times")
