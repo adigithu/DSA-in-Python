2b9b762c98882211059727caeb84825d755fbe72
Aditya J Parida <adityaparida605@gmail.com>
2026-02-10 07:15:03 +0530

    Subsequences of a given list (clean)

---
+++ BAsic_Operations_List.py
@@ -0,0 +1,7 @@
+my_list=[10,20,30,40,50]
+print("The third element in the list is ", my_list[2])
+print("The length of the list is ", len(my_list))
+if len(my_list)==0:
+    print("The list is empty")
+else:
+    print("The list is not empty")
