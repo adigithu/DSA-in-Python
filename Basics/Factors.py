91c40eebdbb95c3e7397dad2f704e26242923c07
Aditya J Parida <adityaparida605@gmail.com>
2026-06-04 11:56:22 +0530

    Factors of a given number

---
+++ Basics/Factors.py
@@ -0,0 +1,9 @@
+from math import *
+n=int(input("Enter a number:"))
+result=[]
+for i in range(1, int(sqrt(n))+1):
+    if n%i==0:
+        result.append(i)
+        if n//i !=i:
+            result.append(n//i)
+print(result)
